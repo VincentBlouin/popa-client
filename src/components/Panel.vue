a6b27475cf47efcc6714bbfdbab28ce03f431fee
Vincent Blouin <vincent.blouin@gmail.com>
2018-08-04 14:39:30 -0400

    mobile padding

--- src/components/Panel.vue
+++ src/components/Panel.vue
@@ -1,17 +1,21 @@
 <template>
-  <v-container fluid>
+  <v-container fluid class='panel pt-2 pb2' :class="{
+                'pl-2 pr-2': $vuetify.breakpoint.mdAndUp,
+                'pl-0 pr-0': $vuetify.breakpoint.smAndDown
+            }">
     <v-slide-y-transition mode="out-in">
       <v-layout row align-center justify-center>
         <v-flex :md8="small" :md12="!small" xs12>
           <div class="white elevation-2">
-            <v-toolbar flat dense color="secondary" dark  >
+            <v-toolbar flat dense color="secondary" dark>
               <v-toolbar-title>
                 {{title}}
               </v-toolbar-title>
             </v-toolbar>
-            <div class="pl-4 pr-4 pt-2 pb-2">
+            <div class="pt-2 pb-2 pl-2 pr-2"
+            >
               <slot>
-                {{noContent || $t("noContent")}}
+                {{noContent || $t('noContent')}}
               </slot>
             </div>
           </div>
